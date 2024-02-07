9376ed86407eb91500dbff979b1afde325867ae3
Pratik Gurung <pratikgurung9497@gmail.com>
2024-02-07 12:37:40 -0500

    commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '0c8b4818d5mshf30561f55f60089p14c477jsn7c0faa6fbe59',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "83389db00cede3c49603794ad5fdba91";
